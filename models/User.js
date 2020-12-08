aad0437ecdf16ecf368b7fa6d6790e3fc8783cf5
Ivan Urra <ivan.urra.wd@gmail.com>
2020-12-08 21:15:45 +0100

    backend ready

--- models/User.js
+++ models/User.js
@@ -21,11 +21,3 @@ const User = mongoose.model('User', userSchema)
 
 module.exports = User
 
-// email: {type: String, required: true},
-//   username: {type: String, required: true},
-//   password: {type: String, required: true, minlength: 8},
-//   runnerType: {type: String, required: true},
-//   experience: {type: String, required: true},
-//   stravaLink: {type: String, required: true},
-//   personalLink: {type: String},
-//   likedRunners: {type: [String]}
