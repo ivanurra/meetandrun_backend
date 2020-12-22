3675ab412ac59efc3c31b865780673c3015756de
Ivan Urra <ivan.urra.wd@gmail.com>
2020-12-22 09:21:50 +0100

    backend

--- models/Like.js
+++ models/Like.js
@@ -1,16 +1,16 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+// const mongoose = require('mongoose')
+// const Schema = mongoose.Schema
 
-const likeSchema = new Schema({
-  runnerLiked: {type: [String]},
-  likedBy: {type: [String]}
-}, {
-    timestamps: {
-      createdAt: 'created_at',
-      updatedAt: 'updated_at'
-    }
-  })
+// const likeSchema = new Schema({
+//   runnerLiked: {type: [String]},
+//   likedBy: {type: [String]}
+// }, {
+//     timestamps: {
+//       createdAt: 'created_at',
+//       updatedAt: 'updated_at'
+//     }
+//   })
 
-const Like = mongoose.model('Like', likeSchema)
+// const Like = mongoose.model('Like', likeSchema)
 
-module.exports = Like
+// module.exports = Like
